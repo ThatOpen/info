2fe4de5e52a2c5b5ed7fc1738464cee99bc8e88d
Ansoni <antoniogviegas@hotmail.com>
2021-07-11 22:19:25 +0200

    First implementation of deepl automatic translator

---
+++ components/scene.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const IfcScene = () => (
+  <iframe
+    width="100%"
+    height="300"
+    key={Math.random()}
+    src="https://ifcjs.github.io/hello-world/introduction/"
+    frameBorder="0"
+  ></iframe>
+);
--- deepl/test.js
+++ deepl/test.js
@@ -2,20 +2,83 @@ const axios = require("axios");
 const fs = require("fs");
 
 const AUTH_ERROR = "Only Ansoni can call the DEEPL API.";
-const PATH_ERROR = "Path for this language not configured yet.";
+const PATH_ERROR = "The path of the file to translate could not be found.";
 const WRITE_ERROR = "An error occured while writing JSON Object to File.";
 const WRITE_OK = "All translations have been successfully saved!";
+
+translateMDX("es", "docs/Introduction.mdx", "Introduction.mdx");
+
+async function translateMDX(lang, route, destination) {
+  if (!fs.existsSync(route)) throw new Error(PATH_ERROR);
+  const lines = readMDX(route);
+  const filtered = filterTexts(lines);
+  // const array =  encodeText(filtered);
+  // const translated = await translateText(array, lang.toUpperCase());
+  const texts = JSON.parse(fs.readFileSync("./deepl/example-response.json"));
+  formatTranslatedTexts(lines, filtered, texts);
+  writeMDXFile(lang, destination, lines);
+}
+
+function writeMDXFile(lang, destination, lines){
+  const path = `./i18n/${lang}/docusaurus-plugin-content-docs/current/` + destination;
+  var file = fs.createWriteStream(path);
+  file.on('error', (err) => console.log(WRITE_ERROR, err));
+  lines.forEach((v) =>  file.write(v + '\n'));
+  file.end();
+}
+
+function formatTranslatedTexts(lines, filtered, translated){
+  const decoded = decodeText(translated);
+  const used = {}
+  let j = 0;
+  for(let i = 0; i < lines.length; i++){
+    if(filtered.has(lines[i])){
+      if(!used[lines[i]]) used[lines[i]] = decoded[j++]; 
+      lines[i] = used[lines[i]];
+    }  
+  }
+}
+
+function decodeText(translated){
+  let result = translated.map(text => text.replace(/^__H1__/, "# "))
+  result = result.map(text => text.replace(/^__H2__/, "## "))
+  result = result.map(text => text.replace(/^__H3__/, "### "))
+  return result;
+}
+
+function encodeText(filtered){
+  let array = Array.from(filtered);
+  array = array.map(text => text.replace(/^# /, "__H1__"))
+  array = array.map(text => text.replace(/^## /, "__H2__"))
+  array = array.map(text => text.replace(/^### /, "__H3__"))
+  return array;
+}
+
+function readMDX(route) {
+  const rawFile = fs.readFileSync(route).toString();
+  return rawFile.split("\n").map((line) => line.replace(/\r/, ""));
+}
+
+function filterTexts(texts) {
+  const filtered = new Set();
+  const filter = /^$|^\s{2,}(?!\/\/)|^---/;
+  texts.forEach((text) => {
+    if (!filter.test(text)) filtered.add(text);
+  });
+  return filtered;
+}
 
 async function translateJSON(lang) {
   const route = `i18n/${lang}/code.json`;
   if (!fs.existsSync(route)) throw new Error(PATH_ERROR);
   const texts = JSON.parse(fs.readFileSync("./deepl/base_code.json"));
-  const translated = await translateText(texts, lang.toUpperCase());
+  const textlist = Object.values(texts).map((text) => text.message);
+  const translated = await translateText(textlist, lang.toUpperCase());
   createTranslatedJson(texts, translated);
   writeResultFile(texts, route);
 }
 
-function createTranslatedJson(texts, translated){
+function createTranslatedJson(texts, translated) {
   const keys = Object.keys(texts);
   for (let i = 0; i < keys.length; i++) {
     texts[keys[i]].message = translated[i];
@@ -49,10 +112,9 @@ async function translateChunks(target, token, chunks) {
 }
 
 function getChunksToTranslate(texts) {
-  const values = Object.values(texts);
-  const allTexts = values.map((text) => `&text=${text.message}`);
+  const allTexts = texts.map((text) => `&text=${text}`);
   let chunks = [];
-  while (allTexts.length > 0) chunks.push(allTexts.splice(0, 50));
+  while (allTexts.length > 0) chunks.push(allTexts.splice(0, 30));
   return chunks.map((chunk) => chunk.join(""));
 }
 
@@ -70,13 +132,11 @@ async function callDeepl(url) {
   }
 }
 
-function getHeaders(){
+function getHeaders() {
   return {
     "Cache-Control": "no-cache",
     "Access-Control-Allow-Origin": "*",
   };
 }
 
-translateJSON("es");
-
-// getTranslation("Hello world!", "DE");
+// translateJSON("es");
