0c5281dcfe8631ae94305b1d95c7ca761e53f603
Ansoni <antoniogviegas@hotmail.com>
2021-10-03 22:30:01 +0200

    Start sponsoring page

---
+++ src/components/Sponsoring/Sponsors.js
@@ -0,0 +1,23 @@
+import ansoni from "../../../static/img/sponsors/antonio.jpg"
+import tom from "../../../static/img/sponsors/tomvandig.png"
+import harry from "../../../static/img/sponsors/harry.jpg"
+
+const Sponsors = [
+    {
+        name: "Ansoni (Maintainer)",
+        source: ansoni,
+        link: "https://www.linkedin.com/in/antonio-gonz%C3%A1lez-viegas-8b2326151/"
+    },
+    {
+        name: "Tom (Maintainer)",
+        source: tom,
+        link: "https://www.linkedin.com/in/tom-van-diggelen-b706a9161/"
+    },
+    {
+        name: "Harry (Maintainer)",
+        source: harry,
+        link: "https://www.linkedin.com/in/harrycollin/"
+    }
+]
+
+export default Sponsors;
